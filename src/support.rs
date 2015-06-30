5d46ac1e1768d2e2bd6bff6fbf509143f97f1925
Ning Sun <sunng@about.me>
2015-06-30 21:49:09 +0800

    (feat) added a simple StringWriter to bridging writer based API and original String based API.

--- src/support.rs
+++ src/support.rs
@@ -1,4 +1,6 @@
 pub mod str {
+    use std::io::{Write, Result};
+
     pub trait SliceChars {
         fn slice_chars_alt(&self, begin: usize, end: usize) -> &str;
     }
@@ -25,6 +27,56 @@ pub mod str {
                 (_, None) => panic!("slice_chars: `end` is beyond end of string"),
                 (Some(a), Some(b)) => unsafe { self.slice_unchecked(a, b) }
             }
+        }
+    }
+
+    pub struct StringWriter {
+        buf: Vec<u8>
+    }
+
+    impl StringWriter {
+        pub fn new() -> StringWriter {
+            StringWriter {
+                buf: Vec::new()
+            }
+        }
+
+        pub fn to_string(self) -> String {
+            if let Ok(s) = String::from_utf8(self.buf) {
+                s
+            } else {
+                String::new()
+            }
+        }
+    }
+
+    impl Write for StringWriter {
+        fn write(&mut self, buf: &[u8]) -> Result<usize> {
+            for b in buf {
+                self.buf.push(*b);
+            }
+            Ok(buf.len())
+        }
+
+        fn flush(&mut self) -> Result<()> {
+            Ok(())
+        }
+    }
+
+    #[cfg(test)]
+    mod test {
+        use support::str::StringWriter;
+        use std::io::Write;
+
+        #[test]
+        fn test_string_writer() {
+            let mut sw = StringWriter::new();
+
+            let _ = sw.write("hello".to_owned().into_bytes().as_ref());
+            let _ = sw.write("world".to_owned().into_bytes().as_ref());
+
+            let s = sw.to_string();
+            assert_eq!(s, "helloworld".to_string());
         }
     }
 }
