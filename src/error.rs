cbdc4e97ab7aa528caa5b7f5bc14e0fa107199ba
Jonathan Nilsson <lord.jonil@gmail.com>
2016-02-21 19:42:56 +0100

    Fixed error printing

--- src/error.rs
+++ src/error.rs
@@ -5,14 +5,14 @@ quick_error! {
     #[derive(Debug, Clone)]
     pub enum TemplateError {
         UnclosedBraces(line_no: usize, col_no: usize) {
-            display("closing braces `}}` expected but EOF reached at line {:?}, column {:?}",
+            display("closing braces `}}}}` expected but EOF reached at line {:?}, column {:?}",
                     line_no, col_no)
-            description("closing braces `}}` expected but EOF reached")
+            description("closing braces `}}}}` expected but EOF reached")
         }
         UnexpectedClosingBraces(line_no: usize, col_no: usize) {
-            display("can't close braces `}}` at line {:?}, column {:?}",
+            display("can't close braces `}}}}` at line {:?}, column {:?}",
                     line_no, col_no)
-            description("can't close braces `}}` at this location")
+            description("can't close braces `}}}}` at this location")
         }
         MismatchingClosedHelper(line_no: usize, col_no: usize, open: String, closed: String) {
             display("helper {:?} was opened, but {:?} is closing at line {:?}, column {:?}",
