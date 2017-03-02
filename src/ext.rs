4cadfc21c48c73036cdcccad36c5d20a7cb5d477
Ning Sun <sunng@about.me>
2017-03-02 22:26:35 +0800

    (refactor) add if-even and if-odd [#126]
    Signed-off-by: Ning Sun <sunng@about.me>

--- src/ext.rs
+++ src/ext.rs
@@ -9,19 +9,45 @@ impl HelperDef for IfCompareHelper {
     fn call(&self, h: &Helper, r: &Registry, rc: &mut RenderContext) -> Result<(), RenderError> {
         let param0 = try!(h.param(0)
                            .map(|p| p.value())
-                           .ok_or_else(|| RenderError::new("Param not found for helper \"ifgt\"")));
+                           .ok_or(RenderError::new("Param not found for helper."))
+                           .and_then(|p| {
+                               p.as_f64()
+                                .ok_or(RenderError::new("Param 0 must be number."))
+                           }));
         let param1 = try!(h.param(1)
-                  .map(|p| p.value())
-                  .ok_or_else(|| RenderError::new("Insuffient params for helper \"ifgt\"")));
+                           .map(|p| p.value())
+                           .ok_or(RenderError::new("Insuffient param for helper."))
+                           .and_then(|p| {
+                               p.as_f64()
+                                .ok_or(RenderError::new("Param 1 must be number."))
+                           }));
 
-        if !(param0.is_number() && param1.is_number()) {
-            return Err(RenderError::new("Params for ifgt must be numbers."));
+        let tmpl = if (self.0)(param0, param1) {
+            h.template()
+        } else {
+            h.inverse()
+        };
+
+        match tmpl {
+            Some(ref t) => t.render(r, rc),
+            None => Ok(()),
         }
+    }
+}
 
-        let param0_f64 = param0.as_f64().unwrap();
-        let param1_f64 = param1.as_f64().unwrap();
+pub struct IfIntHelper(Box<Fn(i64) -> bool + Send + Sync>);
 
-        let tmpl = if (self.0)(param0_f64, param1_f64) {
+impl HelperDef for IfIntHelper {
+    fn call(&self, h: &Helper, r: &Registry, rc: &mut RenderContext) -> Result<(), RenderError> {
+        let param0 = try!(h.param(0)
+                           .map(|p| p.value())
+                           .ok_or(RenderError::new("Param not found for helper"))
+                           .and_then(|p| {
+                               p.as_i64()
+                                .ok_or(RenderError::new("Param must be integer."))
+                           }));
+
+        let tmpl = if (self.0)(param0) {
             h.template()
         } else {
             h.inverse()
@@ -36,10 +62,13 @@ impl HelperDef for IfCompareHelper {
 
 
 pub fn setup_ext_helpers(reg: &mut Registry) {
-    reg.register_helper("ifgt", Box::new(IfCompareHelper(Box::new(|x, y| x > y))));
-    reg.register_helper("iflt", Box::new(IfCompareHelper(Box::new(|x, y| x < y))));
-    reg.register_helper("ifgte", Box::new(IfCompareHelper(Box::new(|x, y| x >= y))));
-    reg.register_helper("iflte", Box::new(IfCompareHelper(Box::new(|x, y| x <= y))));
-    reg.register_helper("ifeq", Box::new(IfCompareHelper(Box::new(|x, y| x == y))));
-    reg.register_helper("ifneq", Box::new(IfCompareHelper(Box::new(|x, y| x != y))));
+    reg.register_helper("if-gt", Box::new(IfCompareHelper(Box::new(|x, y| x > y))));
+    reg.register_helper("if-lt", Box::new(IfCompareHelper(Box::new(|x, y| x < y))));
+    reg.register_helper("if-gte", Box::new(IfCompareHelper(Box::new(|x, y| x >= y))));
+    reg.register_helper("if-lte", Box::new(IfCompareHelper(Box::new(|x, y| x <= y))));
+    reg.register_helper("if-eq", Box::new(IfCompareHelper(Box::new(|x, y| x == y))));
+    reg.register_helper("if-neq", Box::new(IfCompareHelper(Box::new(|x, y| x != y))));
+
+    reg.register_helper("if-even", Box::new(IfIntHelper(Box::new(|x| x % 2 == 0))));
+    reg.register_helper("if-odd", Box::new(IfIntHelper(Box::new(|x| x % 2 == 1))));
 }
