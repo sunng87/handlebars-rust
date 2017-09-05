cbe8fb5cc834aa1aa544a2759770705adbf485db
Ning Sun <sunng@about.me>
2017-09-05 19:28:03 +0800

    (test) added not subexpression test
    Signed-off-by: Ning Sun <sunng@about.me>

--- tests/subexpression.rs
+++ tests/subexpression.rs
@@ -3,6 +3,18 @@ extern crate handlebars;
 extern crate serde_json;
 
 use handlebars::{Handlebars, RenderError, Helper, RenderContext};
+use serde_json::Value;
+
+fn is_truthy(v: &Value) -> bool {
+    match *v {
+        Value::Bool(ref i) => *i,
+        Value::Number(ref n) => n.as_f64().map(|f| f.is_normal()).unwrap_or(false),
+        Value::Null => false,
+        Value::String(ref i) => i.len() > 0,
+        Value::Array(ref i) => i.len() > 0,
+        Value::Object(ref i) => i.len() > 0,
+    }
+}
 
 #[test]
 fn test_subexpression() {
@@ -29,10 +41,75 @@ fn test_subexpression() {
         }),
     );
 
+    hbs.register_helper(
+        "not",
+        Box::new(|h: &Helper,
+         _: &Handlebars,
+         rc: &mut RenderContext|
+         -> Result<(), RenderError> {
+            let p1 = h.param(0).map(|v| is_truthy(v.value())).unwrap_or(false);
+            if !p1 {
+                rc.writer.write("true".as_bytes())?;
+            }
+            Ok(())
+        }),
+    );
+
+    let data = json!({"a": 1, "b": 0, "c": 2});
+
     assert_eq!(
         hbs.template_render(
             "{{#if (gt a b)}}Success{{else}}Failed{{/if}}",
-            &json!({"a": 1, "b": 0}),
+            &data,
+        ).unwrap(),
+        "Success"
+    );
+
+    assert_eq!(
+        hbs.template_render(
+            "{{#if (gt a c)}}Success{{else}}Failed{{/if}}",
+            &data,
+        ).unwrap(),
+        "Failed"
+    );
+
+    assert_eq!(
+        hbs.template_render(
+            "{{#if (not (gt a c))}}Success{{else}}Failed{{/if}}",
+            &data,
+        ).unwrap(),
+        "Success"
+    );
+
+    assert_eq!(
+        hbs.template_render(
+            "{{#if (not (gt a b))}}Success{{else}}Failed{{/if}}",
+            &data,
+        ).unwrap(),
+        "Failed"
+    );
+
+    // no argument provided for not
+    assert_eq!(
+        hbs.template_render(
+            "{{#if (not)}}Success{{else}}Failed{{/if}}",
+            &data,
+        ).unwrap(),
+        "Failed"
+    );
+
+    // json literal
+    assert_eq!(
+        hbs.template_render(
+            "{{#if (not true)}}Success{{else}}Failed{{/if}}",
+            &data,
+        ).unwrap(),
+        "Failed"
+    );
+    assert_eq!(
+        hbs.template_render(
+            "{{#if (not false)}}Success{{else}}Failed{{/if}}",
+            &data,
         ).unwrap(),
         "Success"
     );
