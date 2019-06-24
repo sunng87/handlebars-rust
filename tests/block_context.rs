0257d1d5ce6ba35b5d5829b5735ebcf0dd18d483
Ning Sun <sunng@protonmail.com>
2019-06-24 23:02:16 +0800

    (test) added case for #264

--- tests/block_context.rs
+++ tests/block_context.rs
@@ -13,8 +13,22 @@ fn test_partial_with_blocks() {
     });
 
     let template = "{{#*inline \"test\"}}{{b}};{{/inline}}{{#each a as |z|}}{{> test z}}{{/each}}";
-    assert_eq!(
-        hbs.render_template(template, &data).unwrap(),
-        "1;2;"
-    );
+    assert_eq!(hbs.render_template(template, &data).unwrap(), "1;2;");
+}
+
+#[test]
+fn test_root_with_blocks() {
+    let hbs = Handlebars::new();
+
+    let data = json!({
+        "a": [
+            {"b": 1},
+            {"b": 2},
+        ],
+        "b": 3,
+    });
+
+    let template =
+        "{{#*inline \"test\"}}{{b}}:{{@root.b}};{{/inline}}{{#each a}}{{> test}}{{/each}}";
+    assert_eq!(hbs.render_template(template, &data).unwrap(), "1:3;2:3;");
 }
