4f9b1b27472c17294a37219885e42fb1cfb72782
Ning Sun <sunng@protonmail.com>
2019-09-06 00:01:18 +0800

    (cargo-release) generate docs

---
+++ handlebars/template/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"enum":[["BlockParam",""],["Parameter",""],["TemplateElement",""]],"struct":[["DirectiveTemplate",""],["ExpressionSpec",""],["HelperTemplate",""],["Subexpression",""],["Template","A handlebars template"],["TemplateMapping",""]]});
