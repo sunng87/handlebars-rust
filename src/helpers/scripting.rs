f586760f4fb1434c9c9c084940ffb53224efdb24
Ning Sun <sunng@protonmail.com>
2020-05-14 22:41:17 +0800

    (feat) added support for hash and param access

--- src/helpers/scripting.rs
+++ src/helpers/scripting.rs
@@ -1,3 +1,6 @@
+use std::collections::HashMap;
+use std::iter::FromIterator;
+
 use crate::context::Context;
 use crate::error::RenderError;
 use crate::helpers::HelperDef;
@@ -5,7 +8,7 @@ use crate::json::value::{to_json, ScopedJson};
 use crate::registry::Registry;
 use crate::render::{Helper, RenderContext};
 
-use rhai::{Dynamic, Engine, INT};
+use rhai::{Dynamic, Engine, Scope};
 
 use serde_json::value::Value as Json;
 
@@ -18,29 +21,39 @@ impl HelperDef for ScriptHelper {
     fn call_inner<'reg: 'rc, 'rc>(
         &self,
         h: &Helper<'reg, 'rc>,
-        reg: &'reg Registry<'reg>,
-        ctx: &'rc Context,
-        rc: &mut RenderContext<'reg, 'rc>,
+        _reg: &'reg Registry<'reg>,
+        _ctx: &'rc Context,
+        _rc: &mut RenderContext<'reg, 'rc>,
     ) -> Result<Option<ScopedJson<'reg, 'rc>>, RenderError> {
-        // TODO:
+        let params: Vec<Dynamic> = h.params().iter().map(|p| convert(p.value())).collect();
+        let hash: Dynamic = HashMap::from_iter(
+            h.hash()
+                .iter()
+                .map(|(k, v)| ((*k).to_owned(), convert(v.value()))),
+        )
+        .into();
+
+        let mut scope = Scope::new();
+        scope.push_dynamic("params", params.into());
+        scope.push_dynamic("hash", hash);
+
         let result = self
             .engine
-            .eval::<INT>(&self.script)
+            .eval_with_scope::<Dynamic>(&mut scope, &self.script)
             .map_err(RenderError::from)?;
+        let result_string = result.take_string().unwrap_or_else(|e| e.to_owned());
 
-        Ok(Some(ScopedJson::Derived(to_json(result))))
+        Ok(Some(ScopedJson::Derived(to_json(result_string))))
     }
 }
 
-impl<'reg, 'rc> Into<Dynamic> for &ScopedJson<'reg, 'rc> {
-    fn into(self) -> Dynamic {
-        match self.as_json() {
-            Json::Number(ref n) => Dynamic::from(n),
-            Json::Bool(ref b) => Dynamic::from(b),
-            Json::Null => Dynamic::from(()),
-            Json::String(ref s) => Dynamic::from(s),
-            Json::Array(ref v) => Dynamic::from(v.clone()),
-            Json::Object(ref o) => Dynamic::from(o.clone()),
-        }
+fn convert(j: &Json) -> Dynamic {
+    match j {
+        Json::Number(n) => Dynamic::from(n.clone()),
+        Json::Bool(b) => Dynamic::from(*b),
+        Json::Null => Dynamic::from(()),
+        Json::String(s) => Dynamic::from(s.clone()),
+        Json::Array(ref v) => Dynamic::from(v.clone()),
+        Json::Object(ref o) => Dynamic::from(o.clone()),
     }
 }
