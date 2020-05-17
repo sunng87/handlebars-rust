bec155e069655dd984e36955cf035ffea74834d2
Ning Sun <sunng@protonmail.com>
2020-05-17 08:46:43 +0800

    (test) add script helper example

---
+++ examples/script.rs
@@ -0,0 +1,34 @@
+#[cfg(feature = "script_helper")]
+use std::error::Error;
+
+#[cfg(feature = "script_helper")]
+use handlebars::Handlebars;
+
+#[cfg(feature = "script_helper")]
+#[macro_use]
+extern crate serde_json;
+
+#[cfg(feature = "script_helper")]
+fn main() -> Result<(), Box<dyn Error>> {
+    let mut handlebars = Handlebars::new();
+
+    handlebars.register_template_file("tpl", "./examples/script/template.hbs")?;
+    handlebars.register_script_helper_file("score", "./examples/script/goals.rhai")?;
+
+    let data = json! {
+        [[{
+            "name": "Dortmund",
+            "goals": ["Haaland", "Guerreiro", "Hazard", "Guerreiro"]
+        }, {
+            "name": "Schalke",
+            "goals": []
+        }]]
+    };
+    println!("{}", handlebars.render("tpl", &data)?);
+    Ok(())
+}
+
+#[cfg(not(feature = "script_helper"))]
+fn main() {
+    println!("Please enable feature flag script_helper for this example");
+}
