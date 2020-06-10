c5c77292d7a94e17683fbbc02d5d3170ad1ef5ee
Ning Sun <sunng@protonmail.com>
2020-06-10 22:27:28 +0800

    (feat) add WIP hash support for helper macro

--- tests/helper_macro.rs
+++ tests/helper_macro.rs
@@ -8,6 +8,7 @@ use handlebars::Handlebars;
 handlebars_helper!(lower: |s: str| s.to_lowercase());
 handlebars_helper!(upper: |s: str| s.to_uppercase());
 handlebars_helper!(hex: |v: i64| format!("0x{:x}", v));
+handlebars_helper!(money: |v: i64, cur: str="$"| format!("{}{}.00", cur, v));
 
 #[test]
 fn test_macro_helper() {
