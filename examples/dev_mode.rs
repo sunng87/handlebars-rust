8ff72adadd579623c14ebf4a1e14a29c9e28a0de
Ning Sun <sunng@protonmail.com>
2022-01-27 22:21:53 +0800

    refactor: use tiny_http for devmode example to speed up builds

--- examples/dev_mode.rs
+++ examples/dev_mode.rs
@@ -2,7 +2,7 @@ use std::sync::Arc;
 
 use handlebars::Handlebars;
 use serde_json::json;
-use warp::{self, Filter};
+use tiny_http::{Response, Server};
 
 fn handlebars() -> Handlebars<'static> {
     let mut reg = Handlebars::new();
@@ -16,16 +16,16 @@ fn handlebars() -> Handlebars<'static> {
     reg
 }
 
-#[tokio::main]
-async fn main() {
+fn main() {
     let hbs = Arc::new(handlebars());
-    let route = warp::get().map(move || {
+
+    let server = Server::http("127.0.0.1:3030").expect("Failed to start demo server.");
+    println!("Edit ./examples/dev_mode/template.hbs and request http://localhost:3030 to see the change on the fly.");
+
+    for req in server.incoming_requests() {
         let result = hbs
             .render("tpl", &json!({"model": "t14s", "brand": "Thinkpad"}))
             .unwrap_or_else(|e| e.to_string());
-        warp::reply::html(result)
-    });
-
-    println!("Edit ./examples/dev_mode/template.hbs and request http://localhost:3030 to see the change on the fly.");
-    warp::serve(route).run(([127, 0, 0, 1], 3030)).await;
+        req.respond(Response::from_string(result)).unwrap();
+    }
 }
