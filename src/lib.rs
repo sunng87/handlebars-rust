619869470d12ae06f0ca60b799b7ab4a4e87627b
lovasoa <pere.jobs@gmail.com>
2022-09-24 20:30:51 +0200

    fmt

--- src/lib.rs
+++ src/lib.rs
@@ -394,11 +394,11 @@ pub use self::error::{RenderError, TemplateError};
 pub use self::helpers::{HelperDef, HelperResult};
 pub use self::json::path::Path;
 pub use self::json::value::{to_json, JsonRender, PathAndJson, ScopedJson};
+pub use self::local_vars::LocalVars;
 pub use self::output::{Output, StringOutput};
 pub use self::registry::{html_escape, no_escape, EscapeFn, Registry as Handlebars};
 pub use self::render::{Decorator, Evaluable, Helper, RenderContext, Renderable};
 pub use self::template::Template;
-pub use self::local_vars::LocalVars;
 
 #[doc(hidden)]
 pub use self::serde_json::Value as JsonValue;
