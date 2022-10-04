e24145fe45b03cfd6b1d3031069796e87c783fa1
Ning Sun <sunng@protonmail.com>
2022-10-04 15:20:17 +0800

    chore: remove unmaintained fuzz tests

--- fuzz/fuzz_targets/render_template_no_data.rs
+++
@@ -1,8 +0,0 @@
-#![no_main]
-use libfuzzer_sys::fuzz_target;
-
-fuzz_target!(|data: &str| {
-    let tpl = handlebars::Handlebars::new();
-
-    let _ = tpl.render_template(&data, &Vec::<u32>::new());
-});
