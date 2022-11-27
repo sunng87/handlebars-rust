cb674472e8c6352ca95da5807eaa1f6fb418c487
Ning Sun <sunng@protonmail.com>
2022-11-27 16:31:19 +0800

    feat: provide handlebarsjs option

--- playground/www/index.js
+++ playground/www/index.js
@@ -1,14 +1,30 @@
 import * as hbs from "hbs-playground";
 
-document.getElementById("render").addEventListener('click', (event) => {
+document.getElementById("rust-render").addEventListener('click', (event) => {
     var template = document.getElementById("template").value;
     var data = document.getElementById("data").value;
     var json_data = JSON.parse(data);
 
     try {
         var result = hbs.render(template, json_data);
-        document.getElementById("output").value = result;
+        document.getElementById("rust-output").value = result;
+        document.getElementById("rust-error").innerText = "";
     } catch (e) {
-        console.log(e);
+        document.getElementById("rust-error").innerText = e.toString();
+    }
+});
+
+document.getElementById("js-render").addEventListener('click', (event) => {
+    var template = document.getElementById("template").value;
+    var data = document.getElementById("data").value;
+    var json_data = JSON.parse(data);
+
+    try {
+        var template = Handlebars.compile(template);
+        var result = template(json_data);
+        document.getElementById("js-output").value = result;
+        document.getElementById("js-error").innerText = "";
+    } catch (e) {
+        document.getElementById("js-error").innerText = e.toString();
     }
 });
