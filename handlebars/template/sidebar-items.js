e5c0a9f6bf2f7bc7180ebd08886d6e39af62a75e
sunng87 <sunng87@users.noreply.github.com>
2022-12-31 03:42:24 +0000

    Deploying to gh-pages from @ sunng87/handlebars-rust@e977ce714542a9cdfe17e879aa5c4519a781038d 🚀

--- handlebars/template/sidebar-items.js
+++
@@ -1,1 +0,0 @@
-initSidebarItems({"enum":[["BlockParam",""],["Parameter",""],["TemplateElement",""]],"struct":[["DirectiveTemplate",""],["ExpressionSpec",""],["HelperTemplate",""],["Subexpression",""],["Template","A handlebars template"],["TemplateMapping",""]]});
--- source-files.js
+++
@@ -1,3 +0,0 @@
-var N = null;var sourcesIndex = {};
-sourcesIndex["handlebars"] = {"name":"","dirs":[{"name":"directives","files":["inline.rs","mod.rs"]},{"name":"helpers","files":["helper_boolean.rs","helper_each.rs","helper_if.rs","helper_log.rs","helper_lookup.rs","helper_raw.rs","helper_with.rs","mod.rs"]}],"files":["context.rs","error.rs","grammar.rs","lib.rs","macros.rs","output.rs","partial.rs","registry.rs","render.rs","support.rs","template.rs","value.rs"]};
-createSourceSidebar();
