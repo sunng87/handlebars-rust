f69d3ff2a23aa12f2b60a3b19b20cd8b68e9a8e5
Mark Ellis <m@rkellis.com>
2025-05-13 08:47:03 +0200

    expose DirectorySourceOptionsBuilder & added doc example
    DirectorySourceOptions is `#[non_exhaustive]` which means it cannot be
    created directly by users of the crate.
    
    DirectorySourceOptions is `#[derive(Builder)]` but the resulting
    DirectorySourceOptionsBuilder is not exposed to users of the crate.
    
    Because of this only `DirectorySourceOptions::default()` can be used and
    options cannot be set.
    
    Exposing DirectorySourceOptionsBuilder allows users of this crate to
    build DirectorySourceOptions.

--- src/lib.rs
+++ src/lib.rs
@@ -403,7 +403,7 @@ pub use self::json::value::{to_json, JsonRender, JsonTruthy, PathAndJson, Scoped
 pub use self::local_vars::LocalVars;
 pub use self::output::{Output, StringOutput};
 #[cfg(feature = "dir_source")]
-pub use self::registry::DirectorySourceOptions;
+pub use self::registry::{DirectorySourceOptions, DirectorySourceOptionsBuilder};
 pub use self::registry::{html_escape, no_escape, EscapeFn, Registry as Handlebars};
 pub use self::render::{Decorator, Evaluable, Helper, RenderContext, Renderable};
 pub use self::template::Template;
